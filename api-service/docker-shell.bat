fe0e1dbf048ff17e9baaaace7c8d8e680cdea783
sathvikbhagavan <sathvikbhagavan@gmail.com>
2021-09-04 10:21:19 +0530

    Fixing minor bugs

--- api-service/docker-shell.bat
+++ api-service/docker-shell.bat
@@ -8,7 +8,7 @@ cd api-service
 
 docker build -t %IMAGE_NAME% -f Dockerfile .
 cd ..
-docker run  --rm --name $IMAGE_NAME -ti ^
+docker run  --rm --name %IMAGE_NAME% -ti ^
             --mount type=bind,source="%cd%\api-service",target=/app ^
             --mount type=bind,source="%cd%\persistent-folder",target=/persistent ^
             -p 9500:9000 -e DEV=1 %IMAGE_NAME%
