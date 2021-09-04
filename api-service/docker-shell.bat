a9770ad0833e029f3f099b92e79133bf049c7633
sathvikbhagavan <sathvikbhagavan@gmail.com>
2021-09-04 20:15:23 +0530

    removed cd from docker-shell.bat for api service

--- api-service/docker-shell.bat
+++ api-service/docker-shell.bat
@@ -1,11 +1,4 @@
 SET IMAGE_NAME="api-server"
-SET BASE_DIR= %cd%
-cd ..
-cd persistent-folder
-SET PERSISTENT_DIR= %cd%
-cd ..
-cd api-service
-
 docker build -t %IMAGE_NAME% -f Dockerfile .
 cd ..
 docker run  --rm --name %IMAGE_NAME% -ti ^
